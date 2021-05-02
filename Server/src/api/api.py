ae1d245f23203118c7de88b29699a1e59ace9b8f
seker212 <seker212@gmail.com>
2021-05-02 18:32:17 +0200

    API part 2

--- Server/src/api/api.py
+++ Server/src/api/api.py
@@ -15,6 +15,7 @@ from src.helpers.User import User
 from src.poker.table import Table
 from src.poker.table_players import TablePlayers
 from src.poker.Player import Player
+from src.poker.player_actions import bet_raise, all_in, call, check, fold, ActionEffect
 from src.api.api_schemas import *
 from src.api.api_models import *
 
@@ -53,6 +54,11 @@ class GetTableInfo(MethodResource, Resource):
         table_schema = TableSchema()
         return table_schema.dump(table_model), 200
 
+
+api.add_resource(GetTableInfo, '/table/<tableID>')
+docs.register(GetTableInfo)
+
+
 class GetPlayerCards(MethodResource, Resource):
     @doc(tags=['Get Info'])
     @use_kwargs({'playerID': fields.String()}, location='querystring')
@@ -73,6 +79,10 @@ class GetPlayerCards(MethodResource, Resource):
                 player_cards = [CardSchema().dump(CardModel(c.suit.name, c.rank)) for c in p.hand]
                 return player_cards, 200
 
+api.add_resource(GetPlayerCards, '/table/<tableID>/playercards')
+docs.register(GetPlayerCards)
+
+
 class AddTable(MethodResource, Resource):
     @doc(tags=['Manage'])
     @marshal_with(None, code='200', description='Returns new table\'s tableID as a string' )
@@ -80,6 +90,10 @@ class AddTable(MethodResource, Resource):
         key = generateStringKey()
         not_started_games[key] = []
         return key, 200
+
+api.add_resource(AddTable, '/newtable')
+docs.register(AddTable)
+
 
 class AddPlayerToTable(MethodResource, Resource):
     @doc(tags=['Manage'])
@@ -100,6 +114,10 @@ class AddPlayerToTable(MethodResource, Resource):
         not_started_games[tableID].append(User(key, playerName))
         return key, 200
 
+api.add_resource(AddPlayerToTable, '/newtable/join/<tableID>')
+docs.register(AddPlayerToTable)
+
+
 class NewTablePlayers(MethodResource, Resource):
     @doc(tags=['Manage'])
     @marshal_with(None, code='200', description='Returns new player\'s playerID as a string' )
@@ -110,6 +128,10 @@ class NewTablePlayers(MethodResource, Resource):
         
         table = not_started_games[tableID]
         return str([user.name for user in table]), 200
+
+api.add_resource(NewTablePlayers, '/newtable/players/<tableID>')
+docs.register(NewTablePlayers)
+
 
 class StartTable(MethodResource, Resource):
     @doc(tags=['Manage'])
@@ -132,23 +154,187 @@ class StartTable(MethodResource, Resource):
         del not_started_games[tableID]
         return '', 200
 
-api.add_resource(GetTableInfo, '/table/<tableID>')
-docs.register(GetTableInfo)
-
-api.add_resource(GetPlayerCards, '/table/<tableID>/playercards')
-docs.register(GetPlayerCards)
-
-api.add_resource(AddTable, '/newtable')
-docs.register(AddTable)
-
-api.add_resource(AddPlayerToTable, '/newtable/join/<tableID>')
-docs.register(AddPlayerToTable)
-
-api.add_resource(NewTablePlayers, '/newtable/players/<tableID>')
-docs.register(NewTablePlayers)
-
 api.add_resource(StartTable, '/newtable/start/<tableID>')
 docs.register(StartTable)
+
+class DisconnectPlayer(MethodResource, Resource):
+    @doc(tags=['Manage'])
+    @use_kwargs({'playerID': fields.String()}, location='querystring')
+    @marshal_with(None, code='204', description='Disconnected' )
+    @marshal_with(None, code='403', description='playerID not found')
+    @marshal_with(None, code='404', description='tableID not found')
+    def delete(self, tableID, playerID):
+        table = None
+        if tableID not in not_started_games:
+            if tableID not in started_games:
+                return '', 404
+            else:
+                table = started_games[tableID]
+                if playerID not in [p.user.id for p in table.players.List]:
+                    return '', 403
+                else:
+                    disconnect_player = None
+                    for p in table.players.List:
+                        if p.user.id == playerID:
+                            disconnect_player = p
+                            break
+                    for pot in table.pots:
+                        if disconnect_player in pot.members:
+                            pot.members.remove(disconnect_player)
+                    table.players.List.remove(disconnect_player)
+        else:
+            table = not_started_games[table]
+            if playerID not in [p.id for p in table]:
+                return '', 403
+            else:
+                disconnect_player = None
+                for p in table:
+                    if p.id == playerID:
+                        disconnect_player = p
+                        break
+                table.remove(disconnect_player)
+
+api.add_resource(DisconnectPlayer, '/table/<tableID>/disconnect')
+docs.register(DisconnectPlayer)
+
+class Bet(MethodResource, Resource):
+    @doc(tags=['Player Actions'])
+    @use_kwargs({'playerID': fields.String(), 'ammount': fields.Integer()}, location='querystring')
+    @marshal_with(None, code='200', description='Disconnected' )
+    @marshal_with(None, code='400', description='Move not allowed')
+    @marshal_with(None, code='403', description='playerID not found')
+    @marshal_with(None, code='404', description='tableID not found')
+    def get(self, tableID, playerID, ammount):
+        if tableID not in started_games:
+            return '', 404
+        if playerID not in [p.user.id for p in table.players.List]:
+            return '', 403
+        player = None
+        for p in table.players.List:
+            if p.user.id == playerID:
+                player = p
+                break
+        result = bet_raise(started_games[tableID], player, ammount)
+
+        if result == ActionEffect.OK:
+            return '', 200
+        else:
+            return result.name, 400
+        
+api.add_resource(Bet, '/table/<tableID>/actions/bet')
+docs.register(Bet)
+
+
+class Check(MethodResource, Resource):
+    @doc(tags=['Player Actions'])
+    @use_kwargs({'playerID': fields.String()}, location='querystring')
+    @marshal_with(None, code='200', description='OK' )
+    @marshal_with(None, code='400', description='Move not allowed')
+    @marshal_with(None, code='403', description='playerID not found')
+    @marshal_with(None, code='404', description='tableID not found')
+    def get(self, tableID, playerID, ammount):
+        if tableID not in started_games:
+            return '', 404
+        if playerID not in [p.user.id for p in table.players.List]:
+            return '', 403
+        player = None
+        for p in table.players.List:
+            if p.user.id == playerID:
+                player = p
+                break
+        result = check(started_games[tableID], player)
+
+        if result == ActionEffect.OK:
+            return '', 200
+        else:
+            return result.name, 400
+        
+api.add_resource(Check, '/table/<tableID>/actions/check')
+docs.register(Check)
+
+
+class Fold(MethodResource, Resource):
+    @doc(tags=['Player Actions'])
+    @use_kwargs({'playerID': fields.String()}, location='querystring')
+    @marshal_with(None, code='200', description='OK' )
+    @marshal_with(None, code='400', description='Move not allowed')
+    @marshal_with(None, code='403', description='playerID not found')
+    @marshal_with(None, code='404', description='tableID not found')
+    def get(self, tableID, playerID, ammount):
+        if tableID not in started_games:
+            return '', 404
+        if playerID not in [p.user.id for p in table.players.List]:
+            return '', 403
+        player = None
+        for p in table.players.List:
+            if p.user.id == playerID:
+                player = p
+                break
+        result = fold(started_games[tableID], player)
+
+        if result == ActionEffect.OK:
+            return '', 200
+        else:
+            return result.name, 400
+        
+api.add_resource(Fold, '/table/<tableID>/actions/fold')
+docs.register(Fold)
+
+
+class Call(MethodResource, Resource):
+    @doc(tags=['Player Actions'])
+    @use_kwargs({'playerID': fields.String()}, location='querystring')
+    @marshal_with(None, code='200', description='OK' )
+    @marshal_with(None, code='400', description='Move not allowed')
+    @marshal_with(None, code='403', description='playerID not found')
+    @marshal_with(None, code='404', description='tableID not found')
+    def get(self, tableID, playerID, ammount):
+        if tableID not in started_games:
+            return '', 404
+        if playerID not in [p.user.id for p in table.players.List]:
+            return '', 403
+        player = None
+        for p in table.players.List:
+            if p.user.id == playerID:
+                player = p
+                break
+        result = call(started_games[tableID], player)
+
+        if result == ActionEffect.OK:
+            return '', 200
+        else:
+            return result.name, 400
+        
+api.add_resource(Call, '/table/<tableID>/actions/call')
+docs.register(Call)
+
+
+class AllIn(MethodResource, Resource):
+    @doc(tags=['Player Actions'])
+    @use_kwargs({'playerID': fields.String()}, location='querystring')
+    @marshal_with(None, code='200', description='OK' )
+    @marshal_with(None, code='400', description='Move not allowed')
+    @marshal_with(None, code='403', description='playerID not found')
+    @marshal_with(None, code='404', description='tableID not found')
+    def get(self, tableID, playerID, ammount):
+        if tableID not in started_games:
+            return '', 404
+        if playerID not in [p.user.id for p in table.players.List]:
+            return '', 403
+        player = None
+        for p in table.players.List:
+            if p.user.id == playerID:
+                player = p
+                break
+        result = all_in(started_games[tableID], player)
+
+        if result == ActionEffect.OK:
+            return '', 200
+        else:
+            return result.name, 400
+        
+api.add_resource(AllIn, '/table/<tableID>/actions/allin')
+docs.register(AllIn)
 
 if __name__ == '__main__':
     app.run(port=29345)
